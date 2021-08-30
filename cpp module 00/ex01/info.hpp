b488d45bbe1aa16acbffa8c2926e38544668dea9
seungju lee <seungjulee@seungjuui-MacBookPro.local>
2021-08-30 11:44:58 +0900

    done ex01

--- cpp module 00/ex01/info.hpp
+++ cpp module 00/ex01/info.hpp
@@ -6,7 +6,7 @@
 /*   By: slee2 <slee2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/29 08:24:44 by slee2             #+#    #+#             */
-/*   Updated: 2021/08/29 08:41:47 by slee2            ###   ########.fr       */
+/*   Updated: 2021/08/30 11:43:04 by slee2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,98 @@
 # define INFO_HPP
 
 # include <iostream>
-# include <iomanip>
+// # include <iomanip>
+# include <string>
+
+class data
+{
+	private:
+		std::string	fn[8];
+		std::string ln[8];
+		std::string nick[8];
+		std::string phone[8];
+		std::string secret[8];
+	public:
+		int	index;
+		std::string	search;
+		void	show_info();
+		void	search_info();
+		void	get_info();
+};
+
+void	data::show_info()
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	std::cout << "  index   |first name|last name | nickname " << std::endl;
+	while (i < index)
+	{
+		std::cout << i << "         " << "|";
+		if (fn[i].length() > 10)
+			std::cout << fn[i].substr(0, 9) << "." << "|";
+		else
+		{
+			std::cout << fn[i];
+			j = 0;
+			while (j++ < 10 - fn[i].length())
+				std::cout << " ";
+			std::cout << "|";
+		}
+		if (ln[i].length() > 10)
+			std::cout << ln[i].substr(0, 9) << "." << "|";
+		else
+		{
+			std::cout << ln[i];
+			j = 0;
+			while (j++ < 10 - ln[i].length())
+				std::cout << " ";
+			std::cout << "|";
+		}
+		if (nick[i].length() > 10)
+			std::cout << nick[i].substr(0, 9) << "." << "|";
+		else
+		{
+			std::cout << nick[i];
+			j = 0;
+			while (j++ < 10 - nick[i].length())
+				std::cout << " ";
+		}
+		std::cout << std::endl;
+		i++;
+	}
+}
+
+void	data::search_info()
+{
+	std::cout << "index : ";
+	std::getline(std::cin, search);
+	if (search.length() == 1 && (int)search[0] <= 55 && (int)search[0] >= 48)
+	{
+		search[0] -= 48;
+		std::cout << "first name     - " << fn[(int)search[0]] << std::endl;
+		std::cout << "last name      - " << ln[(int)search[0]] << std::endl;
+		std::cout << "nickname       - " << nick[(int)search[0]] << std::endl;
+		std::cout << "phone number   - " << phone[(int)search[0]] << std::endl;
+		std::cout << "darkest secret - " << secret[(int)search[0]] << std::endl;
+	}
+	else
+		std::cout << "This is incorrect behavior." << std::endl;
+}
+
+void	data::get_info()
+{
+	std::cout << "first name     : ";
+	std::getline(std::cin, fn[index]);
+	std::cout << "last name      : ";
+	std::getline(std::cin, ln[index]);
+	std::cout << "nickname       : ";
+	std::getline(std::cin, nick[index]);
+	std::cout << "phone number   : ";
+	std::getline(std::cin, phone[index]);
+	std::cout << "darkest secret : ";
+	std::getline(std::cin, secret[index]);
+}
 
 #endif
--- cpp module 00/ex01/main.cpp
+++ cpp module 00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: slee2 <slee2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/29 00:21:28 by slee2             #+#    #+#             */
-/*   Updated: 2021/08/29 08:44:21 by slee2            ###   ########.fr       */
+/*   Updated: 2021/08/30 11:35:31 by slee2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,27 @@
 int	main(void)
 {
 	std::string order;
+	data a;
+	a.index = 0;
+
 	while (1)
 	{
 		std::cout << "order : ";
-		std::cin >> order;
+		std::getline(std::cin, order);
 		if (order == "EXIT")
 			break ;
+		else if (order == "ADD")
+		{
+			if (a.index == 8)
+				a.index = 0;
+			a.get_info();
+			a.index++;
+		}
+		else if (order == "SEARCH")
+		{
+			a.show_info();
+			std::cout << std::endl;
+			a.search_info();
+		}
 	}
 }
