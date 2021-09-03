33ae8b36b2f8f08d4951ff78d774cd5e5b05f9b6
seungju0000 <seungju0000@naver.com>
2021-09-03 11:32:01 +0900

    asdf

---
+++ cpp module 01/ex00/Zombie.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slee2 <slee2@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/29 00:21:28 by slee2             #+#    #+#             */
+/*   Updated: 2021/09/01 18:43:02 by slee2            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void    Zombie::announce(void)
+{
+    std::cout << "<" + Zombie::name + "> Braiiii BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::Zombie( std::string g_name )
+{
+    Zombie::name = g_name;
+}
---
+++ cpp module 01/ex00/Zombie.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slee2 <slee2@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/29 00:21:28 by slee2             #+#    #+#             */
+/*   Updated: 2021/09/01 18:43:02 by slee2            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Zombie
+{
+    private:
+        std::string name;
+    public:
+        void    announce();
+        
+        Zombie( std::string g_name );
+        ~Zombie(void);
+};
+
+#endif
