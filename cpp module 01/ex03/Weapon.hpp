9e895132256f9149bd4f330941f5c1a51d770a46
Seungju Lee <slee2@c7r4s5.42seoul.kr>
2021-09-06 14:21:51 +0900

    add ex03

--- cpp module 01/ex03/Weapon.hpp
+++ cpp module 01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: slee2 <slee2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 13:01:50 by slee2             #+#    #+#             */
-/*   Updated: 2021/09/06 13:45:02 by slee2            ###   ########.fr       */
+/*   Updated: 2021/09/06 14:15:07 by slee2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
 class	Weapon
 {
 	private:
-	public:
 		std::string	WEAPON_TYPE;
+	public:
 		const std::string	getType(void);
 		void	setType(std::string set);
 		Weapon(std::string g);
