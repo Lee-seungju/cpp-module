a979145ace97b93cfa6b36c0337e2dfb4139c876
Seungju Lee <slee2@c7r4s5.42seoul.kr>
2021-09-06 20:39:48 +0900

    yaho

---
+++ cpp module 01/ex05/Karen.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Karen.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slee2 <slee2@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/06 19:16:42 by slee2             #+#    #+#             */
+/*   Updated: 2021/09/06 20:38:03 by slee2            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Karen.hpp"
+
+void	Karen::debug(void)
+{
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+}
+
+void	Karen::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+}
+
+void	Karen::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
+}
+
+void	Karen::error(void)
+{
+	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+}
+
+void	Karen::complain(std::string level)
+{
+	check_t	p_level[4] = {
+		{"DEBUG", &Karen::debug},
+		{"INFO", &Karen::info},
+		{"WARNING", &Karen::warning},
+		{"ERROR", &Karen::error}
+	};
+	std::string	whatlevel[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int	i = std::find(whatlevel, whatlevel + 4, level) - whatlevel;
+	
+	switch(i)
+	{
+		case 4:
+			return ;
+	}
+	void	(Karen::*ptr)(void) = p_level[i].d;
+	(this->*ptr)();
+}
