38439901d62ed8e74f5b7463cbd06b25c9f9e76e
seungju lee <seungjulee@seungjuui-MacBookPro.local>
2021-10-20 01:15:22 +0900

    copy file ex01 to ex02

---
+++ cpp module 05/ex02/Bureaucrat.hpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slee2 <slee2@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 21:16:26 by slee2             #+#    #+#             */
+/*   Updated: 2021/10/20 00:26:11 by slee2            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <exception>
+#include "Form.hpp"
+
+class Form;
+
+class Bureaucrat {
+	private:
+		const	std::string		name;
+		int						grade;
+	
+	public:
+		class GradeTooHighException: public std::exception {
+			public:
+				const char* what(void) const throw();
+		};
+		class GradeTooLowException: public std::exception {
+			public:
+				const char* what(void) const throw();
+		};
+
+		Bureaucrat();
+		Bureaucrat(std::string _name, int _grade);
+		Bureaucrat(Bureaucrat const& a);
+		Bureaucrat& operator=(Bureaucrat const &c);
+		~Bureaucrat();
+
+		void				signForm(const Form& f);
+
+		const	std::string&		getName() const;
+		int						getGrade() const;
+		void					increment();
+		void					decrement();
+};
+
+std::ostream&   operator<<(std::ostream &out, const Bureaucrat &b);
+
+#endif
---
+++ cpp module 05/ex02/main.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slee2 <slee2@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 21:30:53 by slee2             #+#    #+#             */
+/*   Updated: 2021/10/20 00:18:27 by slee2            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int main()
+{
+	try {
+		Bureaucrat	a("bb", 100);
+		std::cout << a << std::endl;
+
+		Form		b("ff", 200, 150);
+		std::cout << b << std::endl;
+		
+	} catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	try {
+		Bureaucrat	a("bb", 100);
+		std::cout << a << std::endl;
+
+		Form		b("ff", 50, 0);
+		std::cout << b << std::endl;
+		
+	} catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	
+	std::cout << std::endl;
+
+	try {
+		Bureaucrat	a("bb", 100);
+		std::cout << a << std::endl;
+
+		Form		b("ff", 50, 120);
+		std::cout << b << std::endl;
+		
+		a.signForm(b);
+	} catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	
+	std::cout << std::endl;
+
+	try {
+		Bureaucrat	a("bb", 100);
+		std::cout << a << std::endl;
+
+		Form		b("ff", 50, 80);
+		std::cout << b << std::endl;
+		
+		a.signForm(b);
+	} catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	
+	return 0;
+}
